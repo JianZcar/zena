99360b2382605fe6ca768e1aa9e1f848cfcd167d
JianZcar <esteban.jianzcar@outlook.com>
2025-03-21 23:40:30 +0800

    updated the rm command in install-kernel, updated the image-info

--- build-scripts/base/00-image-info.sh
+++ build-scripts/base/00-image-info.sh
@@ -4,31 +4,31 @@ echo "::group:: ===$(basename "$0")==="
 
 set -ouex pipefail
 
-IMAGE_PRETTY_NAME="Bluefin"
+IMAGE_PRETTY_NAME="Zena"
 IMAGE_LIKE="fedora"
-HOME_URL="https://projectbluefin.io"
-DOCUMENTATION_URL="https://docs.projectbluefin.io"
-SUPPORT_URL="https://github.com/ublue-os/bluefin/issues/"
-BUG_SUPPORT_URL="https://github.com/ublue-os/bluefin/issues/"
-CODE_NAME="Archaeopteryx"
+HOME_URL="https://github.com/JianZcar/zena"
+DOCUMENTATION_URL="https://github.com/JianZcar/zena"
+SUPPORT_URL="https://github.com/JianZcar/zena/issues"
+BUG_SUPPORT_URL="https://github.com/JianZcar/zena/issues"
+CODE_NAME="k1"
 VERSION="${VERSION:-00.00000000}"
 
-IMAGE_INFO="/usr/share/ublue-os/image-info.json"
+IMAGE_INFO="/usr/share/zena/image-info.json"
 IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
 
 # Image Flavor
-image_flavor="main"
+IMAGE_FLAVOR="main"
 if [[ "${IMAGE_NAME}" =~ nvidia ]]; then
-  image_flavor="nvidia"
+  IMAGE_FLAVOR="nvidia"
 fi
 
 cat >$IMAGE_INFO <<EOF
 {
   "image-name": "$IMAGE_NAME",
-  "image-flavor": "$image_flavor",
+  "image-flavor": "$IMAGE_FLAVOR",
   "image-vendor": "$IMAGE_VENDOR",
   "image-ref": "$IMAGE_REF",
-  "image-tag":"$UBLUE_IMAGE_TAG",
+  "image-tag":"$IMAGE_TAG",
   "base-image-name": "$BASE_IMAGE_NAME",
   "fedora-version": "$FEDORA_MAJOR_VERSION"
 }
--- build-scripts/base/01-install-kernel.sh
+++ build-scripts/base/01-install-kernel.sh
@@ -5,9 +5,9 @@ set -oue pipefail
 INSTALLED_KERNEL_PACKAGES="$(rpm -qa --qf "%{NAME}\n" | grep -P '^kernel(?!-tools).*')"
 
 YUM_DIR="/etc/yum.repos.d/"
-REPO="bieszczaders/kernel-cachyos/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo"
-wget -P $YUM_DIR "https://copr.fedorainfracloud.org/coprs/$REPO"
+link="https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo"
+wget -P $YUM_DIR $REPO
 
 rpm-ostree cliwrap install-to-root / && \
 rpm-ostree override remove $INSTALLED_KERNEL_PACKAGES --install=kernel-cachyos-lts
-rm "$YUM_DIR$REPO"
+rm "${YUM_DIR}bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo"
