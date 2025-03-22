ec97020c23f12a88e8798bd13b6587cf97edf627
JianZcar <esteban.jianzcar@outlook.com>
2025-03-22 12:06:55 +0800

    fixed var in install kernel

--- build-scripts/base/01-install-kernel.sh
+++ build-scripts/base/01-install-kernel.sh
@@ -5,8 +5,8 @@ set -oue pipefail
 INSTALLED_KERNEL_PACKAGES="$(rpm -qa --qf "%{NAME}\n" | grep -P '^kernel(?!-tools).*')"
 
 YUM_DIR="/etc/yum.repos.d/"
-link="https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo"
-wget -P $YUM_DIR $REPO
+LINK="https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo"
+wget -P $YUM_DIR $LINK
 
 rpm-ostree cliwrap install-to-root / && \
 rpm-ostree override remove $INSTALLED_KERNEL_PACKAGES --install=kernel-cachyos-lts
