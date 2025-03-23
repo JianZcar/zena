8f69f7c73e087794525401b26e5126688eb11b2b
JianZcar <esteban.jianzcar@outlook.com>
2025-03-23 20:54:00 +0800

    setup copr is now it's own script

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -6,7 +6,8 @@ rpm-ostree install --idempotent dnf5 dnf5-plugins
 echo "::endgroup::"
 
 /ctx/base/00-image-info.sh
-/ctx/base/01-install-kernel-akmods.sh
+/ctx/base/01-setup-copr-repos.sh
+/ctx/base/02-install-kernel-akmods.sh
 
 
 echo "::group:: ===Install MoreWaita==="
