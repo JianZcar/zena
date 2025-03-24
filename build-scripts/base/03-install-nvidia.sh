01c30e8dbd35e5e0d5bba992641778067f0de9a2
JianZcar <esteban.jianzcar@outlook.com>
2025-03-24 12:19:20 +0800

    removed slash

--- build-scripts/base/03-install-nvidia.sh
+++ build-scripts/base/03-install-nvidia.sh
@@ -9,8 +9,9 @@ dnf5 -y install \
     mesa-vdpau-drivers.x86_64 \
     mesa-vdpau-drivers.i686 && \
 curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/hwe/b3a3dbddf4af81cfbfa7526c1918c9b9f014f86b/nvidia-install.sh && \
-chmod +x /tmp/nvidia-install.sh \
-IMAGE_NAME="${BASE_IMAGE_NAME}" /tmp/nvidia-install.sh && \
+chmod +x /tmp/nvidia-install.sh 
+IMAGE_NAME="${BASE_IMAGE_NAME}" 
+/tmp/nvidia-install.sh && \
 rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json && \
 ln -s libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so && \
 dnf5 -y copr disable ublue-os/staging
