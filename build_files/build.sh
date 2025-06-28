c6fee72c5a52694df4297ccf304f138dd3cb8788
JianZcar <esteban.jianzcar@outlook.com>
2025-06-28 14:03:22 +0800

    feat(build): Add package patching and overrides
    This commit introduces a new script, , to handle package swaps, version locking, and installations from various repositories.
    
    The  script has been updated to execute this new script as part of the build process. This modularizes the package management and makes it easier to maintain.

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,11 +19,7 @@ PKGS_TO_UNINSTALL=(
 /ctx/00-repos.sh
 /ctx/01-kernel.sh
 /ctx/02-nvidia.sh
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+/ctx/03-patches.sh
 
 # this installs a package from fedora repos
 if [ ${#PKGS_TO_INSTALL[@]} -gt 0 ]; then
@@ -33,14 +29,5 @@ fi
 if [ ${#PKGS_TO_UNINSTALL[@]} -gt 0 ]; then
     dnf5 remove -y "${PKGS_TO_UNINSTALL[@]}"
 fi
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
 
 systemctl enable podman.socket
