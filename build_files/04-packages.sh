6fd4acb1c95e1c00cd31e5d3a876110342096d1c
JianZcar <esteban.jianzcar@outlook.com>
2025-06-29 10:53:28 +0800

    more packages

--- build_files/04-packages.sh
+++ build_files/04-packages.sh
@@ -8,6 +8,8 @@ shopt -s nullglob
 
 PKGS_TO_INSTALL=(
   # Development Tools
+  nvim
+  tree-sitter
   ripgrep
   udica
 
@@ -91,6 +93,37 @@ fi
 if [ ${#PKGS_TO_UNINSTALL[@]} -gt 0 ]; then
     dnf5 remove -y "${PKGS_TO_UNINSTALL[@]}"
 fi
+
+sed -i 's|uupd|& --disable-module-distrobox|' /usr/lib/systemd/system/uupd.service
+
+dnf5 -y install \
+    "$(curl -s https://api.github.com/repos/bazzite-org/cicpoffs/releases/latest \
+        | jq -r '.assets[] | select(.name | test(".*rpm$")) | .browser_download_url')"
+
+mkdir -p /etc/xdg/autostart
+
+
+sed -i 's/ --xdg-runtime=\\"${XDG_RUNTIME_DIR}\\"//g' /usr/bin/btrfs-assistant-launcher
+
+curl -Lo /usr/bin/installcab https://raw.githubusercontent.com/bazzite-org/steam-proton-mf-wmv/master/installcab.py
+chmod +x /usr/bin/installcab
+
+curl -Lo /usr/bin/install-mf-wmv https://github.com/bazzite-org/steam-proton-mf-wmv/blob/master/install-mf-wmv.sh
+chmod +x /usr/bin/install-mf-wmv
+
+curl -Lo /tmp/ls-iommu.tar.gz "$(curl -s https://api.github.com/repos/HikariKnight/ls-iommu/releases/latest \
+    | jq -r '.assets[] | select(.name | test(".*x86_64.tar.gz$")) | .browser_download_url')"
+mkdir -p /tmp/ls-iommu
+tar --no-same-owner --no-same-permissions --no-overwrite-dir -xvzf /tmp/ls-iommu.tar.gz -C /tmp/ls-iommu
+cp -r /tmp/ls-iommu/ls-iommu /usr/bin/
+rm -rf /tmp/ls-iommu*
+
+SCOPEBUDDY_TAG=$(curl -s https://api.github.com/repos/HikariKnight/scopebuddy/releases/latest | jq -r '.tag_name')
+curl -Lo /tmp/scopebuddy.tar.gz "https://github.com/HikariKnight/scopebuddy/archive/refs/tags/${SCOPEBUDDY_TAG}.tar.gz"
+mkdir -p /tmp/scopebuddy
+tar --no-same-owner --no-same-permissions --no-overwrite-dir -xvzf /tmp/scopebuddy.tar.gz -C /tmp/scopebuddy
+cp -r /tmp/scopebuddy/ScopeBuddy-*/bin/* /usr/bin/
+rm -rf /tmp/scopebuddy*
 
 systemctl enable podman.socket
 
