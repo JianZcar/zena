b289f3b5dc0e177075dbdc7917c35766a4e2d56e
Jian Z'car Esteban <esteban.jianzcar@outlook.com>
2025-07-13 13:28:35 +0800

    Update 00-repos.sh

--- build_files/00-repos.sh
+++ build_files/00-repos.sh
@@ -80,9 +80,11 @@ dnf5 -y config-manager setopt "*staging*".exclude="scx-scheds kf6-* mesa* mutter
 
 # Disable fedora default repo
 mkdir -p /etc/flatpak/remotes.d/
-cat <<EOF > /etc/flatpak/remotes.d/fedora.flatpakrepo
-[Remote]
+tee /etc/flatpak/remotes.d/fedora.flatpakrepo >/dev/null <<'EOF'
+[Flatpak Repo]
+Title=Fedora Flatpak Remote
 Name=fedora
+Url=oci+https://registry.fedoraproject.org
 Disabled=true
 EOF
 
