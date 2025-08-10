f174ff565dad6408324008030f3d0cb3c6144e98
JianZcar <esteban.jianzcar@outlook.com>
2025-08-10 17:04:18 +0800

    remove editing non existent desktop entry

--- build_files/08-finalize.sh
+++ build_files/08-finalize.sh
@@ -8,7 +8,6 @@ cp --no-dereference --preserve=links /usr/lib/libdrm.so.2 /usr/lib/libdrm.so
 cp --no-dereference --preserve=links /usr/lib64/libdrm.so.2 /usr/lib64/libdrm.so
 
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/nvim.desktop
-sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/nvtop.desktop
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/btop.desktop
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/yad-icon-browser.desktop
 
