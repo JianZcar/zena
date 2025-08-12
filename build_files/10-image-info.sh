9632bff1049b0da47003815c770441655a629056
JianZcar <esteban.jianzcar@outlook.com>
2025-08-12 23:52:37 +0800

    removed unbound var

--- build_files/10-image-info.sh
+++ build_files/10-image-info.sh
@@ -7,7 +7,7 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 sed -i "s/^VERSION=.*/VERSION=\"${RELEASE} (Zena)\"/" /usr/lib/os-release
-sed -i "s/^ID=fedora/ID=${IMAGE_PRETTY_NAME}/" /usr/lib/os-release
+sed -i "s/^ID=fedora/ID=zena/" /usr/lib/os-release
 sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME=\"Zena ${RELEASE}.${VERSION_DATE} (Fedora)\"/" /usr/lib/os-release
 echo "OSTREE_VERSION=\"${VERSION_TAG}\"" >> /usr/lib/os-release
 
