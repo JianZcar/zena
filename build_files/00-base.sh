e6b2c057a86b8f327a86ad26527efc392d7760f3
JianZcar <esteban.jianzcar@outlook.com>
2025-12-16 00:10:23 +0800

    group fix

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -155,4 +155,40 @@ packages=(
 )
 pacman -S --noconfirm "${packages[@]}"
 
+cat <<'EOF' > /usr/libexec/group-fix
+#!/bin/sh
+
+cat /usr/lib/sysusers.d/*.conf \
+  | grep -e "^g" \
+  | grep -v -e "^#" \
+  | awk "NF" \
+  | awk '{print $2}' \
+  | grep -v -e "wheel" -e "root" -e "sudo" \
+  | xargs -I{} sed -i "/{}/d" "$1"
+EOF
+chmod +x /usr/libexec/group-fix
+
+cat <<'EOF' > /usr/lib/systemd/system/group-fix.service
+[Unit]
+Description=Fix system groups
+Wants=local-fs.target
+After=local-fs.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/libexec/group-fix /etc/group
+ExecStart=/usr/libexec/group-fix /etc/gshadow
+ExecStart=systemd-sysusers
+
+[Install]
+WantedBy=default.target
+WantedBy=multi-user.target
+EOF
+
+cat <<'EOF' > /usr/lib/systemd/system-preset/01-group-fix.preset
+enable group-fix.service
+EOF
+
+systemctl enable group-fix
+
 echo "::endgroup::"
