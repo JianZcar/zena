da8ddda0a33cbd3df0c7e44448b8a1d4937b82a9
JianZcar <esteban.jianzcar@outlook.com>
2025-12-23 20:29:07 +0800

    install bootupd

---
+++ build_files/03-services.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "::group:: ===$(basename "$0")==="
+
+set -ouex pipefail
+
+shopt -s nullglob
+
+system_services=(
+  group-fix
+  systemd-homed
+  systemd-resolved
+  NetworkManager
+  bluetooth
+  firewalld
+
+  initial-install
+  update-bootc-remote
+
+  podman.socket
+)
+systemctl enable "${system_services[@]}"
+
+echo "::endgroup::"
