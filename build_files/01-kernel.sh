e636888654f8e044bf1198d5b8dd77c3cf70056d
JianZcar <esteban.jianzcar@outlook.com>
2025-12-25 18:39:06 +0800

    use extraction method to install the kernel

--- build_files/01-kernel.sh
+++ build_files/01-kernel.sh
@@ -6,15 +6,18 @@ set -ouex pipefail
 
 dnf5 -y remove --no-autoremove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-tools kernel-tools-libs
 
-packages=(
-  kernel-cachyos
-  kernel-cachyos-devel-matched
-  kernel-cachyos-nvidia-open
-)
 
-dnf5 -y install ${packages[@]} --no-allow-downgrade
+dnf5 download --resolve kernel-cachyos kernel-cachyos-modules kernel-cachyos-nvidia-open
 
-dnf5 versionlock add $packages
+for rpm in *.rpm; do
+  rpm2cpio "$rpm" | cpio -idmv
+done
+
+KVER=$(ls lib/modules | head -n1)
+
+mkdir -p /usr/lib/modules/${KVER}/
+cp boot/vmlinuz-${KVER} /usr/lib/modules/${KVER}/vmlinuz
+cp boot/initramfs-${KVER}.img /usr/lib/modules/${KVER}/initramfs.img
 
 pushd /usr/lib/kernel/install.d
 mv -f 05-rpmostree.install.bak 05-rpmostree.install
