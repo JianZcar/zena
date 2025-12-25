d7a1b779928476bf7cd07d9125358eb756367998
JianZcar <esteban.jianzcar@outlook.com>
2025-12-25 19:17:14 +0800

    fix?

--- build_files/01-kernel.sh
+++ build_files/01-kernel.sh
@@ -25,13 +25,8 @@ dnf5 versionlock add kernel-cachyos \
   kernel-cachyos-devel-matched \
   kernel-cachyos-nvidia-open
 
-pushd /usr/lib/kernel/install.d
-mv -f 05-rpmostree.install.bak 05-rpmostree.install
-mv -f 50-dracut.install.bak 50-dracut.install
-popd
-
-dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
-dnf5 -y install scx-scheds
-dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
-
+# dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
+# dnf5 -y install scx-scheds
+# dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
+#
 echo "::endgroup::"
