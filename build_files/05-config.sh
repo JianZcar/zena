d4ea6220dc461b947dcec2913902321ad0531a18
JianZcar <esteban.jianzcar@outlook.com>
2025-12-26 11:25:09 +0800

    clean up the config

--- build_files/05-config.sh
+++ build_files/05-config.sh
@@ -6,13 +6,6 @@ set -ouex pipefail
 
 /ctx/helper/config-apply.sh
 
-sed -i 's/balanced=balanced$/balanced=balanced-bazzite/' /etc/tuned/ppd.conf
-sed -i 's/performance=throughput-performance$/performance=throughput-performance-bazzite/' /etc/tuned/ppd.conf
-sed -i '/^\[battery\]/a performance=balanced-bazzite' /etc/tuned/ppd.conf
-sed -i 's/balanced=balanced-battery$/balanced=balanced-battery-bazzite/' /etc/tuned/ppd.conf
-
-cp /usr/lib/tuned/profiles/throughput-performance-bazzite/script.sh /usr/lib/tuned/profiles/balanced-bazzite/script.sh
-
 sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
 sed -i 's|^OnUnitInactiveSec=.*|OnUnitInactiveSec=7d\nPersistent=true|' /usr/lib/systemd/system/bootc-fetch-apply-updates.timer
 sed -i 's|#AutomaticUpdatePolicy.*|AutomaticUpdatePolicy=stage|' /etc/rpm-ostreed.conf
@@ -21,9 +14,5 @@ sed -i 's|#LockLayering.*|LockLayering=true|' /etc/rpm-ostreed.conf
 sed -i 's|uupd|& --disable-module-distrobox|' /usr/lib/systemd/system/uupd.service
 
 sed -i '/gnome_keyring.so/ s/-auth/auth/ ; /gnome_keyring.so/ s/-session/session/' /etc/pam.d/greetd
-
-tee /usr/lib/bootc/kargs.d/01-boot.toml <<'EOF'
-kargs = ["console=null", "vt.global_cursor_default=0"]
-EOF
 
 echo "::endgroup::"
