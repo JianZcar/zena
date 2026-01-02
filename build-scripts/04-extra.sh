7ab5654176c8c1eb7bca08aa58812b434afca844
JianZcar <esteban.jianzcar@outlook.com>
2026-01-02 10:21:15 +0800

    on extra allow erasing

--- build-scripts/04-extra.sh
+++ build-scripts/04-extra.sh
@@ -41,7 +41,7 @@ packages=(
   libobs_glcapture.i686
   openxr
 )
-dnf5 -y --allowerasing install "${packages[@]}"
+dnf5 -y install --allowerasing "${packages[@]}"
 
 # Install install_weak_deps=false
 packages=(
