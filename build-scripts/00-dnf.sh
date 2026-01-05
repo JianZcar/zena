b7231b5c6c547e9083fe018c7c7d89fa82b89699
JianZcar <esteban.jianzcar@outlook.com>
2026-01-05 17:19:29 +0800

    added terra

--- build-scripts/00-dnf.sh
+++ build-scripts/00-dnf.sh
@@ -31,6 +31,9 @@ echo -n "max_parallel_downloads=10" >>/etc/dnf/dnf.conf
 dnf5 -y install \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y install --nogpgcheck --repofrompath \
+  'terra,https://repos.fyralabs.com/terra$releasever' \
+  terra-release{,-extras,-mesa}
 
 for copr in "${coprs[@]}"; do
   echo "Enabling copr: $copr"
@@ -43,9 +46,10 @@ done
 
 echo "priority=1" | sudo tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri.repo
 echo "priority=2" | sudo tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:ulysg:xwayland-satellite.repo
-
-dnf5 -y config-manager setopt "fedora-multimedia*".priority=3 "fedora-multimedia*".excludepkgs="mesa-* *xone*"
-dnf5 -y config-manager setopt "*rpmfusion*".priority=4
+dnf5 -y config-manager setopt "*terra*".priority=3 "*terra*".exclude="nerd-fonts topgrade scx-tools scx-scheds steam python3-protobuf"
+dnf5 -y config-manager setopt "terra-mesa".enabled=true
+dnf5 -y config-manager setopt "fedora-multimedia*".priority=4 "fedora-multimedia*".excludepkgs="mesa-* *xone*"
+dnf5 -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-*"
 dnf5 -y config-manager setopt "*fedora*".exclude="mesa-* kernel-core-* kernel-modules-* kernel-uki-virt-*"
 
 dnf5 -y distro-sync
