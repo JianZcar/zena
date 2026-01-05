d56e654c6a4b23dd00d3609eefe09844201e8e3a
JianZcar <esteban.jianzcar@outlook.com>
2026-01-05 17:33:43 +0800

    remove Negativo17 multimedia

--- build-scripts/00-dnf.sh
+++ build-scripts/00-dnf.sh
@@ -20,10 +20,6 @@ coprs=(
   sneexy/zen-browser
 )
 
-repos=(
-  https://negativo17.org/repos/fedora-multimedia.repo
-)
-
 mkdir -p /var/roothome
 dnf5 -y install dnf5-plugins
 echo -n "max_parallel_downloads=10" >>/etc/dnf/dnf.conf
@@ -40,16 +36,11 @@ for copr in "${coprs[@]}"; do
   dnf5 -y copr enable "$copr"
 done
 
-for repo in "${repos[@]}"; do
-    dnf5 -y config-manager addrepo --from-repofile="$repo"
-done
-
 echo "priority=1" | sudo tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri.repo
 echo "priority=2" | sudo tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:ulysg:xwayland-satellite.repo
 dnf5 -y config-manager setopt "*terra*".priority=3 "*terra*".exclude="nerd-fonts topgrade scx-tools scx-scheds steam python3-protobuf"
 dnf5 -y config-manager setopt "terra-mesa".enabled=true
-dnf5 -y config-manager setopt "fedora-multimedia*".priority=4 "fedora-multimedia*".excludepkgs="mesa-* *xone*"
-dnf5 -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-*"
+dnf5 -y config-manager setopt "*rpmfusion*".priority=4 "*rpmfusion*".exclude="mesa-*"
 dnf5 -y config-manager setopt "*fedora*".exclude="mesa-* kernel-core-* kernel-modules-* kernel-uki-virt-*"
 
 dnf5 -y distro-sync
