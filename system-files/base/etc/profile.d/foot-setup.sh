0b45dd8cdac45126983afec027e2ea2756ae4b95
JianZcar <esteban.jianzcar@outlook.com>
2026-01-18 10:15:08 +0800

    automatic create dank-colors.ini

--- system-files/base/etc/profile.d/foot-setup.sh
+++ system-files/base/etc/profile.d/foot-setup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-FOOT_CONFIG="$HOME/.config/foot/foot.ini"
-
+FOOT_DIR="$HOME/.config/foot"
+FOOT_CONFIG="$FOOT_DIR"/foot.ini
+DMS_CONFIG="$FOOT_DIR"/dank-colors.ini
 if [ -f "$FOOT_CONFIG" ]; then
     sed -i "s|<USERNAME>|$USER|g" "$FOOT_CONFIG"
+fi
+if [ ! -e "$DMS_CONFIG" ]; then
+    touch "$DMS_CONFIG"
 fi
